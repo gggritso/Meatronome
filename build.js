383aedff888db52f0ee55b8acca4827965521163
George Gritsouk <george.gritsouk@gmail.com>
2015-01-04 22:53:47 -0500

    fix: Simplify build process.

--- build.js
+++ build.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 var
   bookmarkletify = require( 'bookmarkletify' ),
-  handlebars = require( 'handlebars' ),
-
   fs = require( 'fs' );
 
 var
-  template = handlebars.compile( fs.readFileSync( 'readme.handlebars', 'utf-8' ) ),
   source = fs.readFileSync( 'meatronome.js', 'utf-8' ),
-  output = bookmarkletify( source ),
+  output = bookmarkletify( source );
 
-  readme = fs.writeFileSync( 'readme.md', template({ code: output }) );
-
+fs.writeFileSync( 'bookmarklet.js', output );
 console.log( output );
